8fefe847f3d1f781b551dc08e271054b34ea31c1
rimi <132326530+cutirimi@users.noreply.github.com>
2023-10-10 18:11:46 +0900

    [level unrated] Title: 홀짝 구분하기, Time: 2.34 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.cpp
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(void) {
     int n;
     cin >> n;
-    if(n%2==0) cout<<n<<" is even"<<endl;
-    else cout<<n<<" is odd"<<endl;
+    cout<<n<<(n%2==0 ? " is even":" is odd")<<endl;
+    // if(n%2==0) cout<<n<<" is even"<<endl;
+    // else cout<<n<<" is odd"<<endl;
     return 0;
 }
