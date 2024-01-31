d097bc79a7a54b7adcf9348553bdda6be3973b38
rimi <132326530+cutirimi@users.noreply.github.com>
2024-01-31 19:55:53 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a, b;
+int main()
+{
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+
+	return 0;
+}
