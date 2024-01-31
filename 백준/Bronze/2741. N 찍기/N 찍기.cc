d531b1d08f213193265a927fb56a1afc790c4420
rimi <132326530+cutirimi@users.noreply.github.com>
2024-01-31 19:53:15 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, a = 0;
+int main()
+{
+	cin >> n;
+
+	while (n != a)
+	{
+		cout << a + 1 << '\n';
+		a++;
+	}
+
+	return 0;
+}
