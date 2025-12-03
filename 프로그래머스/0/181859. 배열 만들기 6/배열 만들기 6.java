d6c7a26a411759ff30480e04380c6fee03681722
rimi <132326530+cutirimi@users.noreply.github.com>
2025-12-03 10:26:27 +0900

    [level 0] Title: 배열 만들기 6, Time: 37.59 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int i = 0;
+
+        while (i < arr.length) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            } else if (!stk.isEmpty() && stk.get(stk.size() - 1) == arr[i]) {
+                stk.remove(stk.size() - 1);
+                i++;
+            } else {
+                stk.add(arr[i]);
+                i++;
+            }
+        }
+        
+        if(stk.isEmpty()) {
+            stk.add(-1);
+        }
+        
+
+        return stk;
+    }
+}
