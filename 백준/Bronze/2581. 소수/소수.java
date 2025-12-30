ecd162bff1ad74778c372657ea178b9614e7ecbb
rimi <132326530+cutirimi@users.noreply.github.com>
2025-12-30 14:39:40 +0900

    [Bronze II] Title: 소수, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int sum = 0, min = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        for (int i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                if (min == 0) {
+                    min = i;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+        sc.close();
+    }
+}
